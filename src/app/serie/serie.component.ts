7df1718e941cc4171a43d9f8b50bd38c1f930f0f
GabyOlver <127071903+GabyOlver@users.noreply.github.com>
2023-09-25 18:59:12 -0600

    commit final

--- src/app/serie/serie.component.ts
+++ src/app/serie/serie.component.ts
@@ -81,18 +81,4 @@ export class SerieComponent {
   
 }
 
-const serie = new SerieComponent();
-
-console.log('1', serie.serie(1)) // (2 * 0) * (3 * 0) + (7 * 0) = 0
-console.log('2', serie.serie(2)) // (2 * 0) * (3 * 0) + (7 * 2) = 14
-console.log('3', serie.serie(3)) // (2 * 0) * (3 * 1) + (7 * 3) = 21
-console.log('4', serie.serie(4)) // (2 * 1) * (3 * 3) + (7 * 5) = 2 * 9 + 35 = 53
-console.log('5', serie.serie(5)) // (2 * 1) * (3 * 6) + (7 * 7) = 2 * 18 + 49 = 85
-console.log('6', serie.serie(6)) // (2 * 2) * (3 * 10) + (7 * 11) = 4 * 30 + 77 = 197
-console.log('7', serie.serie(7)) // (2 * 3) * (3 * 15) + (7 * 13) = 6 * 45 + 91 = 361
-console.log('8', serie.serie(8)) // (2 * 5) * (3 * 21) + (7 * 17) = 10 * 63 + 119 = 749
-console.log('9', serie.serie(9)) // (2 * 8) * (3 * 28) + (7 * 19) = 16 * 84 + 133 = 1477
-console.log('10', serie.serie(10)) // (2 * 13) * (3 * 36) + (7 * 23) = 26 * 108 + 161 = 2969
-
-
 
